0af33519033b8c26a10dc737c94f373874d11017
Shreyasi Saha <139678159+ShreyasiSaha1@users.noreply.github.com>
2024-11-10 13:34:16 +0530

    Delete Backend/Backend directory

--- Backend/Backend/config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB=async()=>{
-     await mongoose.connect('mongodb+srv://shreyasisaha74:yTbAZDLwfBk5gmCT@cluster0.csm3m.mongodb.net/food-delivery').then(()=>{
-          console.log("DB connected");
-     })
-}
